818e44ef8dac00fc3debaba62f6152415643f680
Gali-Madhan-Kumar <gmadhan99@gmail.com>
2020-04-10 00:49:38 +0530

    displays error message if Incorrect username and/or password

--- src/screens/login/Login.js
+++ src/screens/login/Login.js
@@ -19,6 +19,34 @@ const styles = {
     }
 }
 class Login extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            username: "",
+            password: "",
+            incorrectUsernamePassword: "dispNone",
+            loggedIn: sessionStorage.getItem('access-token') == null ? false : true
+        }
+    }
+
+    usernameChangeHandler = (e) => {
+        this.setState({ username: e.target.value })
+    }
+
+    passwordChangeHandler = (e) => {
+        this.setState({ password: e.target.value })
+    }
+
+    loginClickHandler = () => {
+        if (this.state.username === "user" && this.state.password === "password") {
+            sessionStorage.setItem('access-token', '8661035776.d0fcd39.39f63ab2f88d4f9c92b0862729ee2784');
+            this.setState({ loggedIn: true });
+        } else {
+            this.setState({ incorrectUsernamePassword: "dispBlock" });
+        }
+    }
+
     render() {
         return (
             <div>
@@ -29,13 +57,14 @@ class Login extends Component {
                             <Typography variant="h5">LOGIN</Typography><br />
                             <FormControl style={styles.formControl} required>
                                 <InputLabel htmlFor="username">Username</InputLabel>
-                                <Input id="username" type="text" placeholder="Username" />
+                                <Input id="username" type="text" username={this.state.username} onChange={this.usernameChangeHandler}/>
                             </FormControl><br /><br />
                             <FormControl style={styles.formControl} required>
                                 <InputLabel htmlFor="password">Password</InputLabel>
-                                <Input id="password" type="text" placeholder="Password" />
+                                <Input id="password" type="text" password={this.state.password} onChange={this.passwordChangeHandler}/>
                             </FormControl><br /><br />
-                            <Button id="login-button" variant="contained" color="primary">LOGIN</Button>
+                            <div className={this.state.incorrectUsernamePassword}><span className="red">Incorrect username and/or password</span></div>
+                            <Button id="login-button" variant="contained" color="primary" onClick={this.loginClickHandler}>LOGIN</Button>
                         </CardContent>
                     </Card>
                 </div>
