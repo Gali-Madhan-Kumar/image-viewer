ec67111bf10877fe5fb353fd8fedeca240d99fa9
Gali-Madhan-Kumar <gmadhan99@gmail.com>
2020-04-09 23:16:24 +0530

    created login card and added textfields for username and password inside login card

--- src/screens/login/Login.js
+++ src/screens/login/Login.js
@@ -1,11 +1,43 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './Login.css';
 import Header from '../../common/header/Header';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import Input from '@material-ui/core/Input';
 
+const styles = {
+    card: {
+        padding: '60px',
+        width: '400px'
+    },
+    formControl: {
+        width: '100%'
+    }
+}
 class Login extends Component {
     render() {
-        return(
-            <Header />
+        return (
+            <div>
+                <Header />
+                <div className="login-card">
+                    <Card style={styles.card}>
+                        <CardContent>
+                            <Typography variant="h5">LOGIN</Typography><br />
+                            <FormControl style={styles.formControl} required>
+                                <InputLabel htmlFor="username">Username</InputLabel>
+                                <Input id="username" type="text" placeholder="Username" />
+                            </FormControl><br /><br />
+                            <FormControl style={styles.formControl} required>
+                                <InputLabel htmlFor="password">Password</InputLabel>
+                                <Input id="password" type="text" placeholder="Password" />
+                            </FormControl>
+                        </CardContent>
+                    </Card>
+                </div>
+            </div>
         )
     }
 }
